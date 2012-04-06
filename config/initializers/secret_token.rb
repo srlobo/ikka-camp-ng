d53826fffba897fcf7be717acb5c0f0e9d13c10c
Félix Ortega Hortigüela <guile@darkstar.local>
2012-04-06 22:34:41 +0200

    Version primera

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IkkaCamp::Application.config.secret_token = '7c8e534e0d0b7cd4ba3ff20eb4a28a00735fdc3f1c0855a190d523dd169c6bb3d212f9c06f6c0ef86ea8e7e77399377480afc78cff2fa2e360c66ca1f88ef69f'
